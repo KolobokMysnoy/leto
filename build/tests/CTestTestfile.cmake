eddbbf6bf7a2188c7b4bad049a2d9b24b1319b41
KolobokMysnoy <Inkasator200@ya.ru>
2023-07-28 10:14:46 +0300

    Remove extra folders

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/g/my_programs/DockerC/translit/tests
-# Build directory: /mnt/g/my_programs/DockerC/translit/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/g/my_programs/DockerC/translit/build/tests/hello_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
--- build/tests/hello_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/mnt/g/my_programs/DockerC/translit/build/tests/hello_test[1]_tests.cmake")
-  include("/mnt/g/my_programs/DockerC/translit/build/tests/hello_test[1]_tests.cmake")
-else()
-  add_test(hello_test_NOT_BUILT hello_test_NOT_BUILT)
-endif()
